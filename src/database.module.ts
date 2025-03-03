3686375d412f717744dbabd15e6671d725ffa749
rsirm <roman.sirmanov@bd.com>
2025-03-03 12:29:49 +0200

    feat: create databse module

---
+++ src/database.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.POSTGRES_HOST || 'localhost',
+      port: Number(process.env.POSTGRES_PORT) || 5432,
+      username: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+      database: process.env.POSTGRES_DB,
+      autoLoadEntities: true,
+      synchronize: true,
+      migrations: ['dist/migrations/*.js'],
+      migrationsRun: true,
+    }),
+  ],
+})
+export class DatabaseModule {}
